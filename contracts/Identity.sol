0c4a252a4eb58895b829dd6d20d06dda116ed8f3
Publius <publius@blockcodex.com>
2017-11-19 21:02:54 -0500

    This is code that compiles - needs testing

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -2,12 +2,17 @@ pragma solidity ^0.4.4;
 
 contract Identity {
   //bytes32 [16] public identities;
-  mapping(unit => hash) public identities;
+  mapping(uint => bytes32) public identities;
 
+  struct Multihash {
+    bytes32 hash;
+    uint8 hash_function;
+    uint8 size;
+  }
 
   // set an Identity
-  function setIdentity(uint profileID, hash userHash) public returns (hash) {
-    require(profileID >= 0 && profileID <= 15);
+  function setIdentity(uint profileID, bytes32 userHash) public returns (bytes32) {
+    //require(profileID >= 0 && profileID <= 15);
 
     identities[profileID] = userHash;
 
@@ -15,8 +20,8 @@ contract Identity {
   }
 
   // Retrieving the userHash
-  function getIdentity(uint profileID) public constant returns (hash) {
-    require(profileID >= 0 && profileID <= 15);
+  function getIdentity(uint profileID) public constant returns (bytes32) {
+    //require(profileID >= 0 && profileID <= 15);
 
     return identities[profileID];
   }
